11052fc3a3b20f9c6560f22d2593baea61cf59fc
ach2ashes <achraf.sbai2012@gmail.com>
2022-12-18 15:42:02 +0100

    file tail source

--- build.sbt
+++ build.sbt
@@ -4,3 +4,4 @@ val AkkaVersion = "2.5.23"
 libraryDependencies += "org.scala-lang" % "scala-library" % "2.13.8"
 libraryDependencies += "com.typesafe.akka" %% "akka-stream" % AkkaVersion
 libraryDependencies += "com.typesafe.akka" %% "akka-stream-typed" % "2.7.0"
+libraryDependencies += "com.lightbend.akka" %% "akka-stream-alpakka-file" % "5.0.0"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,8 +9,9 @@ import java.nio.file.Paths
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{FileIO, Flow, Sink, Source}
-
+import akka.stream.scaladsl.{FileIO,Flow, Sink, Source}
+import akka.stream.alpakka.file.scaladsl.FileTailSource
+import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import java.lang
 
@@ -22,21 +23,29 @@ object LogFileAnalyzer {
 
     // Read the log file as a source of lines
     val logFile = Paths.get("./src/main/scala/log-generator.log")
-    val source :  Source[ByteString, Future[IOResult]]= FileIO.fromPath(logFile).via(Framing.delimiter(ByteString("\n"), maximumFrameLength = 256, allowTruncation = true))
-    // Parse each line of the log file to extract the website name
-    val websiteFlow = Flow[ByteString].map { line =>
+    val source =   FileTailSource(logFile,maxChunkSize = 4096,startingPosition=0L,pollingInterval = 250.millis).via(Framing.delimiter(ByteString("\n"), maximumFrameLength = 4096, allowTruncation = true)).map { line =>
       val fields = line.utf8String.split(" ")
       val website = fields(2)
       (website,1)
-    }.groupBy(Int.MaxValue,_._1).fold(("",0)) { case ((_, count), (website, _)) => (website, count + 1) }
-.map { case (website, count) => (website, count) }.mergeSubstreams
-  
+    }.groupBy(Int.MaxValue,_._1)//.fold(0){_+_._2}
+    //.reduce { (entry1, entry2) =>val (website1, visits1) = entry1 
+      //val (website2, visits2) = entry2
+     // (website1, visits1 + visits2)}
 
+    //.map { count=> s"(website, $count)" }
+    .mergeSubstreams
+    
+
+
+    // Parse each line of the log file to extract the website name
+    val websiteFlow = Flow[ByteString]
+    
+  
+val sink = FileIO.toPath(Paths.get("./f.txt")) 
     // Group the website names and count the number of visits for each website
     //case (website, visits) => (website, visits +1)
-    source.via(websiteFlow).runWith(Sink.foreach {
-      case (website, count) => println(s"$website: $count")
-    })
+val f=    source.runWith(Sink.foreach(println))
+Await.result(f,10.second)
 }
  
 }
